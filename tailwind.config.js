f18a5d3dfc9f0907939b527252c2eb3175a9f5e5
vinsdesign <kelvinsaputra024@gmail.com>
2023-11-08 17:11:29 +0700

    feat: navbar travelbucks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      fontFamily: {
+        'sans': ["Poppins"],
+      },
+    },
   plugins: [],
 }
-
+}
